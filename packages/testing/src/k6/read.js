0a04546a6d7e40cff0921ad8be8f7990f710c647
deepsoumyaaa <soumyadeepghosh617@gmail.com>
2026-07-27 00:17:40 +0530

    feat(tests): refactor K6 load testing script to use stages and thresholds for better performance metrics

--- packages/testing/src/k6/read.js
+++ packages/testing/src/k6/read.js
@@ -1,13 +1,21 @@
 import http from "k6/http";
 import { sleep, check } from "k6";
+import { getTodos } from "../lib/api.js";
 
 export const options = {
-  vus: 50,
-  duration: "30s",
+  stages: [
+    { duration: "10s", target: 50 },
+    { duration: "20s", target: 50 },
+    { duration: "10s", target: 0 },
+  ],
+  thresholds: {
+    http_req_duration: ["p(95)<300"],
+    http_req_failed: ["rate<0.01"],
+  },
 };
 
 export default function () {
-  const res = http.get("http://todo-app.local/api/todos");
+  const res = getTodos()
 
   check(res, {
     "status is 200": (r) => r.status === 200,
