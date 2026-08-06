29198e6189d8d96d00abf99fd9bd7a9af8a8e5ab
deepcfr <soumyadeepghosh617@gmail.com>
2026-08-06 23:57:05 +0530

    chore(monitoring): update dashboard naming, disable default rules, add Telegram alerts, tune Prometheus for local testing, and adjust k6 alert test

--- packages/testing/src/k6/hpa.js
+++ packages/testing/src/k6/hpa.js
@@ -4,9 +4,9 @@ import http from "k6/http";
 
 export const options = {
   vus: 20,
-  duration: "1m",
+  duration: "2m",
 };
 
 export default function () {
-  http.get("http://todo-app.local/api/debug/cpu?BIG_VALUE=1000000000");
+  http.get("http://todo-app.local/api/debug/cpu?BIG_VALUE=100000");
 }
